d96337abba2bf99e39206410c8fd3a6601a2edd7
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-12 04:29:58 -0500

    Update api.php

--- api.php
+++ api.php
@@ -10,7 +10,7 @@ try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Connection failed:fff " . $e->getMessage());
 }
 
 // Handle GET requests
